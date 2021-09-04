b31498fd0f48dff59ed8c50d48527c825100db90
anushkasahoo <85781997+anushkasahoo@users.noreply.github.com>
2021-09-04 19:18:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload() {
+}
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(img,300,220,150,120);
+    
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    rect(600,150,20,250);
+      
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    rect(150,150,20,250);
+      
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    rect(150,150,450,20);
+      
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    rect(150,400,450,20);
+      
+    fill(0,255, 0);
+    stroke(128, 0, 0);
+    ellipse(160, 160, 55, 55);
+      
+    fill(0,255, 0);
+    stroke(128, 0, 0);
+    ellipse(610, 160, 55, 55);
+    
+    fill(0,255, 0);
+    stroke(128, 0, 0);
+    ellipse(610, 410, 55, 55);
+      
+    fill(0,255, 0);
+    stroke(128, 0, 0);
+    ellipse(160, 410, 55, 55);
+}
+function take_snapshot() {
+    save('student_name.png');
+}
